b7eea4d983c9c683cff1ce13dd902533a4f4de2d
Elyashiv <Elyashiv2468@gmail.com>
2022-03-13 21:14:24 +0200

    Added FireFox and Edge tests

--- src/test/java/test/java/DockerTests.java
+++ src/test/java/test/java/DockerTests.java
@@ -1,37 +1,89 @@
 package test.java;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
 
 public class DockerTests {
     WebDriver driver;
-    static final String APP_URL = "https://www.google.com/";
+    static final String APP_URL = "https://parabank.parasoft.com/";
     static final String HOTS_URL = "http://localhost:4444";
 
-    @BeforeMethod
-    public void setUp() throws MalformedURLException {
-        ChromeOptions opt = new ChromeOptions();
-        driver = new RemoteWebDriver(new URL(HOTS_URL), opt);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.navigate().to(APP_URL);
+    @Test
+    public void contactViaChrome() {
+        try {
+            ChromeOptions opt = new ChromeOptions();
+            driver = new RemoteWebDriver(new URL(HOTS_URL), opt);
+            driver.navigate().to(APP_URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Starting contact test through Docker via Chrome");
+        test();
+        System.out.println("Chrome test finished");
     }
 
     @Test
-    public void assertTitle() {
-        String expTitle = "Google";
-        System.out.println("Actual title result " + driver.getTitle());
-        Assert.assertTrue(driver.getTitle().contains(expTitle));
+    public void contactViaFireFox() {
+        try {
+            FirefoxOptions opt = new FirefoxOptions();
+            driver = new RemoteWebDriver(new URL(HOTS_URL), opt);
+            driver.navigate().to(APP_URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Starting contact test through Docker via FireFox");
+        test();
+        System.out.println("FireFox test finished");
     }
 
+    @Test
+    public void contactViaEdge() {
+        try {
+            EdgeOptions opt = new EdgeOptions();
+            driver = new RemoteWebDriver(new URL(HOTS_URL), opt);
+            driver.navigate().to(APP_URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Starting contact test through Docker via Edge");
+        test();
+        System.out.println("Edge test finished");
+    }
+
+    private void test() {
+        WebElement contactClick = driver.findElement(By.linkText("contact"));
+
+        System.out.println("ParaBank main page opened.");
+        contactClick.click();
+
+        System.out.println("Contact Us clicked");
+        // Fill all the required fields
+        driver.findElement(By.id("name")).sendKeys("Dor Cohen");
+        driver.findElement(By.id("email")).sendKeys("A7X@gmail.com");
+        driver.findElement(By.id("phone")).sendKeys("0500000000");
+        driver.findElement(By.id("message")).sendKeys("Test");
+        driver.findElement((By.xpath("(//input[@class='button'])[2]"))).click();
+
+        // Verification
+        System.out.println("Starting Verification");
+        String actVerificationText = driver.findElement(By.cssSelector("#rightPanel > p:nth-child(2)")).getAttribute("innerText");
+        String expVerificationText = "Thank you Dor Cohen";
+        Assert.assertEquals(actVerificationText, expVerificationText, "Actual verification message is not as expected. \nActual: " + actVerificationText + "\nExpected: " + expVerificationText);
+
+        // Close browser
+        driver.quit();
+    }
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
